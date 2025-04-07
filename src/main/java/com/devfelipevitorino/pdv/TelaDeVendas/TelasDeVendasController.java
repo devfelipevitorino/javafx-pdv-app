06c5672d481d872c55c2611d45f50346f292b3e6
devfelipevitorino <devfelipevitorino@gmail.com>
2025-04-06 23:21:52 -0300

    adicionando acao ao botao sair na tela de vendas

--- src/main/java/com/devfelipevitorino/pdv/TelaDeVendas/TelasDeVendasController.java
+++ src/main/java/com/devfelipevitorino/pdv/TelaDeVendas/TelasDeVendasController.java
@@ -1,4 +1,49 @@
 package com.devfelipevitorino.pdv.TelaDeVendas;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.util.Optional;
+
 public class TelasDeVendasController {
+
+    @FXML
+    private Button btn_sair;
+
+    @FXML
+    private void fecharTelaVendas() throws IOException {
+
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.initStyle(StageStyle.TRANSPARENT);
+        alerta.setTitle("Sair");
+        alerta.setHeaderText("Deseja sair?");
+
+        ButtonType buttonSim = new ButtonType("Sim");
+        ButtonType buttonNao = new ButtonType("Não");
+
+        alerta.getButtonTypes().setAll(buttonSim, buttonNao);
+
+        Optional<ButtonType> resultado = alerta.showAndWait();
+
+        if (resultado.isPresent() && resultado.get() == buttonSim) {
+            Stage stage = (Stage) btn_sair.getScene().getWindow();
+            stage.close();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/devfelipevitorino/pdv/login-view.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage login = new Stage();
+            login.initStyle(StageStyle.UTILITY);
+            login.setScene(new Scene(root));
+            login.centerOnScreen();
+            login.show();
+        }
+    }
 }
